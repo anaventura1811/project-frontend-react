eb39cc56e4eb2afca5f1b3d0a23cad92f60c7584
Ana Ventura <anaventura1811@gmail.com>
2021-04-27 17:05:20 -0300

    Conceito de props

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,22 @@ import ControlPanel from './componentes/ControlPanel/ControlPanel';
 import Header from './componentes/Header/Header';
 import TodoList from './componentes/ToDoList/Todolist';
 import './styles/global.css';
+import styles from './styles/app.module.css'
+
+function Mensagem(props) {
+  console.log(props);
+  const conteudo = 'Brasil';
+  return (
+    <h2 className={styles.mensagem}>Bom dia, meu povo do {conteudo}!</h2>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <Header />
       <TodoList />
+      <Mensagem frase={'Olá, meu povo!'}/>
       <ControlPanel />
       <Button />
     </div>
@@ -20,3 +30,5 @@ function App() {
 export default App;
 
 // Duas formas de criar componente: função e classe 
+// Classe usa o método render
+// Props - forma de passar parâmetro pra função no React
